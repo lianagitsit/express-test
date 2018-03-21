9b241fa19a43ba9a6fe69179fce3bcc025a93944
Liana Mancini <liana.mancini@gmail.com>
2018-03-21 15:25:20 -0400

    add port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello world'))
-app.listen(3000, () => console.log("Listening on 3000"))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
